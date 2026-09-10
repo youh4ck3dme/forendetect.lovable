721b60dc8efbdefda68cdb4fbcdfefb8a98672ef
youh4ck3dme <H4CK3D@H4CK3D.ME>
2026-09-11 00:08:29 +0200

    fix(deploy): update vercel.json output to dist with no-cache headers and sync release to main & development

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* Forendo PWA Service Worker */
-const CACHE_NAME = "forendo-cache-v1";
+const CACHE_NAME = "forendo-cache-v3";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
